6e5046f5e12c2b32bf1551c527dd2ea7edf44a83
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-11 21:50:00 +0300

    adds startup settings

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <the-navbar></the-navbar>
+  <div class="container with-nav">
+    <login-page></login-page>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "./components/TheNavbar";
+import LoginPage from "./views/LoginPage";
+
+export default {
+  components: { TheNavbar, LoginPage },
+  provide() {
+    return {
+      emails: [
+        { id: 1, theme: "Купил себе PlayStation 5" },
+        { id: 2, theme: "Выучил Vue Router" },
+        { id: 3, theme: "Хочу изучить весь Vue" },
+        { id: 4, theme: "А следующий блок про Vuex!" },
+        { id: 5, theme: "А что там на счет Vue Hooks?" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
