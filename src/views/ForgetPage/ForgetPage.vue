e184f99df7d2332d0beda50ae0db78778b4d03ea
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-08-12 13:52:12 +0300

    use router-link

--- src/views/ForgetPage/ForgetPage.vue
+++ src/views/ForgetPage/ForgetPage.vue
@@ -2,6 +2,19 @@
   <div class="card">
     <h2>Забыл пароль? Так вспоминай!</h2>
 
-    <button class="btn" >Обратно к логину</button>
+    <button class="btn" @click="backToLogin">Обратно к логину</button>
+    <button class="btn" @click="$router.push('/login')">
+      Обратно к логину 2
+    </button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    backToLogin() {
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
